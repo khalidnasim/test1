5143b1852be1e4c8fe92214fd5eb0a8f7b81d585
Khalid <80971727+khalidnasim@users.noreply.github.com>
2026-04-28 12:29:58 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Success!</h1><p>Your Python app is running on Vercel.</p>"
+
+@app.route('/about')
+def about():
+    return "This is the about page."
+
+# Vercel will look for the 'app' variable
